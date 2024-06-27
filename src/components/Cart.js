2e1b30002ac3abe02c27dda8ee9dcbf62d306366
alok kumar <alok60541@gmail.com>
2024-06-27 13:30:26 +0530

    bill details logic done

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,14 +3,48 @@ import { CDN_URL } from "../utils/constants";
 import { clearCart, clearItem, decreaseItemCount, increaseItemCount } from "../utils/cartSlice";
 import { MdDeleteForever } from "react-icons/md";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Cart = () => {
+  const [itemTotalAmount, setItemTotalAmount] = useState(0);
+  const [amountToPay, setAmountToPay] = useState(0);
   const cartItems = useSelector((store) => store.cart.items);
   const resNeededInfo = useSelector((store) => store.cart.restaurant);
   const dispatch = useDispatch();
 
   console.log("cartitems ", cartItems);
   console.log("resdata ", resNeededInfo);
+
+  // to calculate total amount of items only
+  useEffect(() => {
+    function getItemTotalAmount() {
+      let total = 0;
+
+      if (cartItems.length !== 0) {
+        cartItems.forEach((item) => {
+          total += (item.card.info.price / 100 || item.card.info.defaultPrice / 100) * (item.quantity)
+        })
+      }
+      total = Number(total.toFixed(2));
+      setItemTotalAmount(total);
+    }
+
+    getItemTotalAmount();
+  }, [cartItems]);
+
+  // to calulate overall amountToPay after adding all extra charges
+  useEffect(() => {
+    function getTotalAmountToPay() {
+      let total = 0;
+      let deliveryFee = (resNeededInfo?.feeDetails?.totalFee / 100 || 0);
+      let gst = Number((itemTotalAmount * 0.18).toFixed(2));
+
+      total = (itemTotalAmount + deliveryFee + gst);
+      setAmountToPay(total);
+    }
+
+    getTotalAmountToPay();
+  }, [itemTotalAmount]);
 
 
   return cartItems.length !== 0 ? (
@@ -68,14 +102,16 @@ const Cart = () => {
                       +
                     </div>
                   </div>
-                  <p>₹ {cartItem.quantity * (cartItem.card.info.price / 100 || cartItem.card.info.defaultPrice / 100)}</p>
+                  <p>
+                    ₹ {(cartItem.quantity * (cartItem.card.info.price / 100 || cartItem.card.info.defaultPrice / 100)).toFixed(2)}
+                  </p>
                 </div>
               </div>
             </div>
           ))}
         </div>
 
-        <div className="w-full flex justify-between border-2 border-blue-500">
+        <section className="w-full flex justify-between border-2 border-blue-500">
           <button
             className="py-2 px-4 bg-red-500 text-white"
             onClick={() => dispatch(clearCart())}
@@ -86,7 +122,28 @@ const Cart = () => {
               Add more Items
             </Link>
           </button>
-        </div>
+        </section>
+
+        <section className="w-full border-2 border-pink-600">
+          <p className="text-sm font-bold">Bill Details</p>
+
+          <div className="flex justify-between text-sm">
+            <p>Item Total</p>
+            <p>₹{itemTotalAmount}</p>
+          </div>
+          <div className="flex justify-between text-sm">
+            <p>Delivery Fee | {resNeededInfo.sla.lastMileTravelString}</p>
+            <p>₹ {(resNeededInfo.feeDetails.totalFee / 100) || 0}</p>
+          </div>
+          <div className="flex justify-between text-sm">
+            <p>GST and Restaurant Charges</p>
+            <p>₹ {(itemTotalAmount * 0.18).toFixed(2)}</p>
+          </div>
+          <div className="flex justify-between text-base  border-t-2 border-[#000] mt-3 pt-1 font-bold">
+            <p>To Pay</p>
+            <p>₹ {amountToPay}</p>
+          </div>
+        </section>
       </div>
     </div>
   ) : (
